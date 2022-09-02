92cdab14841b3f36b4954cb5a26a33ac18d18c36
account0125 <54588130+account0125@users.noreply.github.com>
2022-09-03 03:14:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,19 +9,19 @@ const imgtotal = 4,
 
 let slidenum = 1;
 
-activeimgelement.setAttribute('src', 'https://raw.githubusercontent.com/account0125/imgslide/main/img/img-1-A.JPG')
+activeimgelement.setAttribute('src', 'https://raw.githubusercontent.com/account0125/imgslide/main/img/img-1-A.jpg')
 imgdataelement.textContent =`8/${slidenum}`
 
 for (let i; i < imgtotal; i++) {
     const lielement = document.createElement('li')
-    lielement.style.backgroundImage = `url(./img/img-${i + 1}-A.jpg)`
+    lielement.style.backgroundImage = `url(https://raw.githubusercontent.com/account0125/imgslide/main/img/img-${i + 1}-A.jpg)`
     imglistelement.appendChild(lielement)
 }
 
 previmgelement.addEventListener('click', () => {
     if (slidenum !== 1) {
         slidenum--
-        activeimgelement.setAttribute('src', `img/img-${slidenum}-A.jpg`)
+        activeimgelement.setAttribute('src', `https://raw.githubusercontent.com/account0125/imgslide/main/img/img-${slidenum}-A.jpg`)
         imgdataelement.textContent =`8/${slidenum}`
     }
 })
@@ -29,17 +29,17 @@ previmgelement.addEventListener('click', () => {
 nextimgelement.addEventListener('click', () => {
     if (slidenum !== imgtotal) {
         slidenum++
-        activeimgelement.setAttribute('src', `./img/img-${slidenum}-A.jpg`)
+        activeimgelement.setAttribute('src', `https://raw.githubusercontent.com/account0125/imgslide/main/img/img-${slidenum}-A.jpg`)
         imgdataelement.textContent =`8/${slidenum}`
     }
 })
 
 beforeimgelement.addEventListener('click', () => {
-    activeimgelement.setAttribute('src', `./img/img-${slidenum}-A.jpg`)
+    activeimgelement.setAttribute('src', `https://raw.githubusercontent.com/account0125/imgslide/main/img/img-${slidenum}-A.jpg`)
     imgdataelement.textContent =`8/${slidenum}`
 })
 
 afterimgelement.addEventListener('click', () => {
-    activeimgelement.setAttribute('src', `./img/img-${slidenum}-B.jpg`)
+    activeimgelement.setAttribute('src', `https://raw.githubusercontent.com/account0125/imgslide/main/img/img-${slidenum}-B.jpg`)
     imgdataelement.textContent =`8/${slidenum}`
 })
