a6a7ec58debdf9afb524d9c28eabc11b73b87289
account0125 <54588130+account0125@users.noreply.github.com>
2022-09-03 02:02:13 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const imgtotal = 4,
+    activeimgelement = document.getElementById("activeimg-img"),
+    imglistelement = document.getElementById('imglist'),
+    previmgelement = document.getElementById('previmg'),
+    nextimgelement = document.getElementById('nextimg'),
+    beforeimgelement = document.getElementById('beforeimg'),
+    afterimgelement = document.getElementById('afterimg'),
+    imgdataelement = document.getElementById('imgdata');
+
+let slidenum = 1;
+
+activeimgelement.setAttribute('src', './img/img-1-A.jpg')
+imgdataelement.textContent =`8/${slidenum}`
+
+for (let i; i < imgtotal; i++) {
+    const lielement = document.createElement('li')
+    lielement.style.backgroundImage = `url(./img/img-${i + 1}-A.jpg)`
+    imglistelement.appendChild(lielement)
+}
+
+previmgelement.addEventListener('click', () => {
+    if (slidenum !== 1) {
+        slidenum--
+        activeimgelement.setAttribute('src', `./img/img-${slidenum}-A.jpg`)
+        imgdataelement.textContent =`8/${slidenum}`
+    }
+})
+
+nextimgelement.addEventListener('click', () => {
+    if (slidenum !== imgtotal) {
+        slidenum++
+        activeimgelement.setAttribute('src', `./img/img-${slidenum}-A.jpg`)
+        imgdataelement.textContent =`8/${slidenum}`
+    }
+})
+
+beforeimgelement.addEventListener('click', () => {
+    activeimgelement.setAttribute('src', `./img/img-${slidenum}-A.jpg`)
+    imgdataelement.textContent =`8/${slidenum}`
+})
+
+afterimgelement.addEventListener('click', () => {
+    activeimgelement.setAttribute('src', `./img/img-${slidenum}-B.jpg`)
+    imgdataelement.textContent =`8/${slidenum}`
+})
