269add6e8a75657f5b87a51b286e67f657662ea0
まいあかうんと <54588130+account0125@users.noreply.github.com>
2022-09-15 21:33:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,55 @@
-const imgtotal = 4,
-    activeimgelement = document.getElementById("activeimg-img"),
-    imglistelement = document.getElementById('imglist'),
-    previmgelement = document.getElementById('previmg'),
-    nextimgelement = document.getElementById('nextimg'),
-    beforeimgelement = document.getElementById('beforeimg'),
-    afterimgelement = document.getElementById('afterimg'),
-    imgdataelement = document.getElementById('imgdata');
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    preventLinks: false,
+    scrollContainer: true,
+    speed: 1000,
+    touchRatio: 0.7,
+    grabCursor: true,
+    parallax: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
 
-let slidenum = 1;
+swiperslideelement = document.getElementById(`background-img1`);
+swiperslideelement.setAttribute("src", `https://raw.githubusercontent.com/account0125/Web-File/main/img-1-A.jpg`);
+swiperslideelement = document.getElementById(`background-img2`);
+swiperslideelement.setAttribute('src', `https://raw.githubusercontent.com/account0125/Web-File/main/img-2-A.jpg`);
 
-activeimgelement.setAttribute('src', 'https://raw.githubusercontent.com/account0125/imgslide/main/img/img-1-A.JPG')
-imgdataelement.textContent =`8/${slidenum}`
+window.onload = function () {
+    setTimeout(() => {
+        const loader = document.getElementById('loader');
+        const notloader = document.getElementById('notloader');
+        loader.classList.add("loaded");
+        notloader.classList.add("endloaded");
+    }, 1200);
+};
 
-previmgelement.addEventListener('click', () => {
-    if (slidenum !== 1) {
-        slidenum--
-        activeimgelement.setAttribute('src', `https://raw.githubusercontent.com/account0125/imgslide/main/img/img-${slidenum}-A.JPG`)
-        imgdataelement.textContent =`8/${slidenum}`
+swiper.on('slideChange', function () {
+    if (swiper.previousIndex < swiper.snapIndex && swiper.snapIndex < 4) {
+        swiperslideelement = document.getElementById(`background-img${swiper.snapIndex + 2}`);
+        swiperslideelement.setAttribute("src", `https://raw.githubusercontent.com/account0125/Web-File/main/img-${swiper.snapIndex + 2}-A.jpg`);
     }
-})
-
-nextimgelement.addEventListener('click', () => {
-    if (slidenum !== imgtotal) {
-        slidenum++
-        activeimgelement.setAttribute('src', `https://raw.githubusercontent.com/account0125/imgslide/main/img/img-${slidenum}-A.JPG`)
-        imgdataelement.textContent =`8/${slidenum}`
+    else if (swiper.previousIndex > swiper.snapIndex && swiper.snapIndex > 2) {
+        swiperslideelement = document.getElementById(`background-img${swiper.snapIndex - 2}`);
+        swiperslideelement.setAttribute("src", `https://raw.githubusercontent.com/account0125/Web-File/main/img-${swiper.snapIndex - 2}-A.jpg`);
     }
-})
+});
 
-beforeimgelement.addEventListener('click', () => {
-    activeimgelement.setAttribute('src', `https://raw.githubusercontent.com/account0125/imgslide/main/img/img-${slidenum}-A.JPG`)
-    imgdataelement.textContent =`8/${slidenum}`
-})
+function imgopen() {
+    imgpopupelement = document.getElementById("imgpopup");
+    imgpopupimgelement = document.getElementById("imgpopup-img")
+    imgpopupimgelement.setAttribute("src", `https://raw.githubusercontent.com/account0125/Web-File/main/img-${swiper.snapIndex + 1}-A.jpg`)
+    imgpopupelement.style.visibility = "visible";
+};
 
-afterimgelement.addEventListener('click', () => {
-    activeimgelement.setAttribute('src', `https://raw.githubusercontent.com/account0125/imgslide/main/img/img-${slidenum}-B.JPG`)
-    imgdataelement.textContent =`8/${slidenum}`
-})
+function answer() {
+    imgpopupimgelement.setAttribute("src", `https://raw.githubusercontent.com/account0125/Web-File/main/img-${swiper.snapIndex + 1}-B.jpg`)
+    imgpopupelement.style.visibility = "visible";
+}
+
+function imgclose() {
+    imgpopupelement = document.getElementById("imgpopup");
+    imgpopupelement.style.visibility = "hidden";
+};
