77134ef27a3e551073cd4d60e0419c8db4874c07
まいあかうんと <54588130+UESHITA-FMHP@users.noreply.github.com>
2022-10-27 17:19:36 +0900

    Update main.js

--- main.js
+++ main.js
@@ -8,8 +8,17 @@ window.onload = function () {
 };
 
 VanillaTilt.init(document.querySelector(".card"), {
-    max: 7,
+    max: 10,
     speed: 400,
     glare:true,
     "max-glare":.5,
 });
+
+function fakelogin() {
+    setTimeout(() => {
+        const fakelogin = document.getElementById('fake-login');
+        const notfakelogin = document.getElementById('not-fake-login');
+        fakelogin.classList.add("fake-login-loaded");
+        notfakelogin.classList.add("fake-login-endloaded");
+    },200);
+}
