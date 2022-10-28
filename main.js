e38f8064f245e12945a39679805a5e1445395dcc
まいあかうんと <54588130+UESHITA-FMHP@users.noreply.github.com>
2022-10-28 18:17:21 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,12 +7,25 @@ window.onload = function () {
     }, 1200);
 };
 
-VanillaTilt.init(document.querySelector(".card"), {
-    max: 10,
-    speed: 400,
-    glare:true,
-    "max-glare":.5,
-});
+function device() {
+    var ua = navigator.userAgent;
+    if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
+        return 'mobile';
+    } else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
+        return 'tablet';
+    } else {
+        return 'desktop';
+    }
+}
+
+if (device() === "desktop") {
+    VanillaTilt.init(document.querySelector(".card"), {
+        max: 15,
+        speed: 2000,
+        glare: true,
+        "max-glare": .5,
+    });
+}
 
 function fakelogin() {
     setTimeout(() => {
@@ -20,5 +33,5 @@ function fakelogin() {
         const notfakelogin = document.getElementById('not-fake-login');
         fakelogin.classList.add("fake-login-loaded");
         notfakelogin.classList.add("fake-login-endloaded");
-    },200);
+    }, 200);
 }
